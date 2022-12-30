fce48e19e3d561c3003495ab280811a6aac347b0
M. Nindra Zaka <mnindrazaka@gmail.com>
2022-12-30 10:00:01 +0700

    feat: setup ts loader

---
+++ webpack.config.client.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  target: "web",
+  entry: "./src/client/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist", "public"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+      filename: path.resolve(__dirname, "dist", "public", "index.html"),
+    }),
+  ],
+};
---
+++ webpack.config.server.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  target: "node",
+  entry: "./src/server/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    filename: "server.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
